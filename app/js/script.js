c527c2e549f7900c812ba935b71000847ec2e030
dovis721 <dovis721@gmail.com>
2020-11-18 19:29:35 +0200

    ale baigiau forn-page, zaidziu su bootstrapu dabar

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
 console.log('hey');
+
+// OWL CAROUSEL ADDED
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+  });
+
+  var owl = $('.owl-carousel');
+    owl.owlCarousel({
+    rtl:true,
+    items:5,
+    loop:true,
+    margin:70,
+    autoplay:false,
+    autoplayTimeout:6000,
+    autoplayHoverPause:true,
+    autoHeight:true
+});
+
